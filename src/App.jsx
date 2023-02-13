bb71ed96397b150d69cecdf457fc9afa0b87ad29
Gideon Odiokine <gidysmartodiokineh@gmail.com>
2023-02-13 09:57:44 +0100

    added: tensorflow to detect object when uploaded

---
+++ src/App.jsx
@@ -0,0 +1,29 @@
+import logo from "./logo.svg";
+import "./App.css";
+import styled from "styled-components";
+import { ObjectDetector } from "./components/objectDetector";
+
+const AppContainer = styled.div`
+	width: 100%;
+	display: flex;
+	/* flex-direction: column; */
+	align-items: center;
+	justify-content: center;
+	color: #fff;
+	gap: 40px;
+`;
+
+function App() {
+	return (
+		<>
+			<div className='group'>
+				<h2>Text and Shapes To Speech Converter </h2>
+			</div>
+			<AppContainer>
+				<ObjectDetector />
+			</AppContainer>
+		</>
+	);
+}
+
+export default App;
